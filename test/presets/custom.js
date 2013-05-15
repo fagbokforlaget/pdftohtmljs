8627570381f1c8c0a0598fcf2fcb991d25ae9547
Deepak Thukral <iapain@gmail.com>
2013-05-15 22:15:12 +0200

    removed unrecognized options from shipped preset file.

--- test/presets/custom.js
+++ test/presets/custom.js
@@ -1,7 +1,6 @@
 exports.load = function(pdf2htmlex) {
     pdf2htmlex.add_options([
         '--zoom 1.33',
-        '--font-format woff',
         '--font-suffix .woff'
     ]);
     return pdf2htmlex
